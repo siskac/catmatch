4bbefe6c6153373986f4ffdc6a501ac868a1f3e1
siskac <siska.charlotte@gmail.com>
2018-09-30 11:31:37 -0600

    adding initial python scripts

---
+++ cat_images.py
@@ -0,0 +1,60 @@
+from google_images_download import google_images_download
+
+cat_breedTuples = [('maine_coon', 'maine coon'), ('norwegian_forest', 'norwegian forest'), \
+		   ('russian_blue', 'russian blue'), ('selkirk_rex', 'selkirk rex'), \
+		   ('siberian', 'siberian cat'), ('siamese', 'siamese cat'), \
+		   ('egyptian_mau', 'egyptian mau'), ('american_shorthair', 'american shorthair cat'), \
+		   ('cornish_rex', 'cornish rex'), ('british_shorthair', 'british shorthair cat'), \
+		   ('scottish_fold', 'scottish fold cat'), ('ragdoll', 'ragdoll cat'), \
+		   ('persian', 'persian cat'), ('devon_rex', 'devon rex'), \
+		   ('abyssinian', 'abyssinian cat'), ('munchkin', 'munchkin cat'), \
+		   ('sphynx', 'sphynx cat'), ('turkish_angora', 'turkish angora'), ('bengal', 'bengal cat'), \
+		   ('birman', 'birman cat'), ('savannah', 'savannah cat'), ('himalayan', 'himalayan cat'), \
+		   ('exotic', 'exotic shorthair'), ('burmese', 'burmese cat'), \
+		   ('manx', 'manx cat'), ('balinese', 'balinese cat'), ('ragamuffin', 'ragamuffin cat')]
+# bombays are black burmese cats
+# cats to def have point coloration: balinese, himalayan, siamese, birman
+# persians are one color
+# abysinnians are always ticked
+# sphynx cat - do not look into coat
+# always spotted: egyptian mau
+
+coat_pointTuples = [('seal_point', 'seal point cat'), ('lilac_point', 'lilac point cat'), \
+		    ('blue_point', 'blue point cat'), ('chocolate_point', 'chocolate point cat'), \
+		    ('red_pont', 'red point cat'), ('cream_point', 'cream point cat')]
+
+coat_patternTuples = [('tuxedo', 'tuxedo cat'), ('tortoise', 'tortoise cat'), ('van', 'van cat'), \
+		      ('spotted_tabby', 'spotted tabby cat'), ('mackeral_tabby', 'mackeral tabby cat'), \
+		      ('calico', 'calico cat'), ('harlequin', 'harlequin coat cat'), \
+		      ('snowshoe', 'snowshoe cat'), ('classic_tabby', 'classic tabby cat'), \
+		      ('van', 'van cat'), ('ticked_tabby', 'ticked tabby cat')] 
+# point coats will be combo of coat_pointTuples
+
+coat_colorTuples = [('red', 'red color cat'), ('chocolate', 'chocolate color cat'), ('cream', 'cream color cat'), \
+		    ('black', 'black cat'), ('lilac', 'lilac cat'), ('blue', 'blue cat'), ('fawn', 'fawn cat'), \
+		    ('smoke', 'smoke cat'), ('white', 'white coat cat')]
+
+response = google_images_download.googleimagesdownload()
+
+
+
+global arguments
+global path
+
+arguments = {'keywords': '', 'limit': 500, 'image_directory': '', 'output_directory': '', 'chromedriver': '/usr/local/bin/chromedriver'}
+path = '/Users/siskac/repos/catmatch/images/'
+
+def catImages(catTuples, type_dir):
+	arguments['output_directory'] = path + type_dir 
+	for cat in catTuples:
+		print cat[1]
+		arguments['keywords'] = cat[1]
+		arguments['image_directory'] = cat[0]
+		try:
+			absolute_image_paths = response.download(arguments)
+		except:
+			pass
+
+catImages(cat_breedTuples, 'breed')
+catImages(coat_pointTuples, 'coat_point')
+catImages(coat_colorTuples, 'coat_color')
---
+++ main.py
@@ -0,0 +1,5 @@
+from split_data import *
+
+# filter out weird files, split data into training and testing
+prepareBuildDir()
+moveFiles('coat_color/')
