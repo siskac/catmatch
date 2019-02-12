d3ebfe80a7fdba12449280850af7291d5a6e4b16
siskac <siska.charlotte@gmail.com>
2019-02-11 18:07:56 -0700

    training code

--- createGraphs.R
+++ createGraphs.R
@@ -1,18 +1,13 @@
-library(gridExtra)
 library(ggplot2)
+
+# total grid search
 
 data <- read.csv(file = "param_search_coat_color_pattern_multi.csv")
 
-plt.acc = ggplot(data, aes(x = epochs, y = val_acc)) + facet_grid(.~factor(learning_rate)) + geom_line(aes(colour = factor(decay))) + 
-		 labs(colour="decay") + ylab("validation accuracy")
-plt.loss = ggplot(data, aes(x = epochs, y = val_loss)) + facet_grid(.~factor(learning_rate)) + geom_line(aes(colour = factor(decay))) + 
-		  labs(colour="decay") + ylab("validation loss")
+plot.loss = ggplot(data, aes(x = epochs, y = val_loss)) + facet_wrap(factor(decay)~factor(learning_rate), scale = "free_y") + 
+			geom_smooth(method = "loess", aes(colour = factor(filter_size)), se = FALSE, alpha = 0.5) + 
+                        ylab("validation loss") + guides(fill=guide_legend(title="Filter Size"))
 
-grid.arrange(plt.acc, plt.loss)
-
-plt.acc.zoomedin = ggplot(data[data$learning_rate < 0.005,], aes(x = epochs, y = val_acc)) + facet_grid(.~factor(learning_rate)) + 
-			  geom_line(aes(colour = factor(decay))) + labs(colour="decay") + ylab("validation accuracy")
-plt.loss.zoomedin = ggplot(data[data$learning_rate < 0.005,], aes(x = epochs, y = val_loss)) + facet_grid(.~factor(learning_rate)) + 
-                           geom_line(aes(colour = factor(decay))) + labs(colour="decay") + ylab("validation loss")
-
-grid.arrange(plt.acc.zoomedin, plt.loss.zoomedin)
+plot.acc =  ggplot(data, aes(x = epochs, y = val_acc)) + facet_wrap(factor(decay)~factor(learning_rate), scale = "free_y") + 
+                        geom_smooth(method = "loess", aes(colour = factor(filter_size)), se = FALSE, alpha = 0.5) + 
+                        ylab("validation accuracy") + guides(fill=guide_legend(title="Filter Size"))
--- param_search.py
+++ param_search.py
@@ -1,28 +1,40 @@
 import pandas as pd
 import numpy as np
 from createNN import runModel_multiclass
+import itertools
+from preprocessing import kerasImages_multiclass
 
-def paramSearch():
-    lrList = [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 1e-1, 5e-1]
-    bsList = [32, 64, 80, 96]
-    decayList = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4]
-
-    BS = 32
-    IMAGE_DIMS = (128, 128, 3)
-
+def paramSearch(dir_path, lrList, decayList, bsList, imagedimList, filterList):
+    possible_combos = list(itertools.product(*[lrList, decayList, bsList, filterList]))
+    epochs = 8
     paramSearch = []
-
-    for lr in lrList:
-        INIT_LR = lr
-        for d in decayList:
-            DECAY = d
+    model_name = 'test'
+    for imagedim in imagedimList:
+        print "Image Size: ", imagedim
+        data, labels = kerasImages_multiclass(dir_path, imagedim)
+        for combos in possible_combos:
+            print combos
+            learning_rate, decay, batch_size, filter_size = combos
+            data, labels = kerasImages_multiclass(dir_path, imagedim)
             try:
-                model, H, trainX, testX, trainY, testY, mlb = runModel_multiclass(data, labels, 'pattern')
-                lr_decay_Dict = {'loss': H.history["loss"], 'acc': H.history["acc"], 'val_loss': H.history["val_loss"], \
-                                 'val_acc': H.history["val_acc"], 'learning_rate': np.repeat(lr, EPOCHS), 'decay': np.repeat(d, EPOCHS),
-                                 'epochs': range(0, EPOCHS)}
-                paramSearch.append(pd.DataFrame(lr_decay_Dict))
+                vals = runModel_multiclass(data, labels, model_name, decay, learning_rate, batch_size, imagedim, epochs, filter_size)
+                model, H, trainX, testX, trainY, testY, mlb = vals
+                param_Dict = {'loss': H.history["loss"], 'acc': H.history["acc"], 'val_loss': H.history["val_loss"], \
+                                 'val_acc': H.history["val_acc"], 'learning_rate': np.repeat(learning_rate, epochs), \ 
+                                 'decay': np.repeat(decay, epochs), 'batch_size': np.repeat(batch_size, epochs), \
+                                 'image_dim': np.repeat(imagedim, epochs), \
+                                 'filter_size': np.repeat(filter_size, epochs), 'epochs': range(0, epochs)}
+                paramSearch.append(pd.DataFrame(param_Dict))
             except:
                 pass
     return pd.concat(paramSearch)
 
+lrList = [3e-3, 3e-2, 0.3, 3]
+decayList = [1e-6, 1e-5, 1e-4]
+bsList = [32]
+imagedimList = [96]
+filterList = [16, 32, 64]
+path = '/Users/siskac/repos/whatcat/images/coat_color_pattern_multi/'
+
+paramPD = paramSearch(dir_path, lrList, decayList, bsList, imagedimList, filterList)
+paramPD.to_csv("paramSearch_coat_color_pattern_multi.csv")
